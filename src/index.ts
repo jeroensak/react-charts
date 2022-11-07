cc7c5c4f97998d400d77da8913d1bea6b80a110e
Jeroen Sak <jeroen.sak@hotmail.com>
2022-11-07 11:58:10 +0100

    export ReactChartsConfigContext, ReactChartsConfigProvider

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,13 @@ import { LineChart } from './components/line-chart';
 import { BarChart } from './components/bar-chart/bar-chart';
 import { StackedBarChart } from './components/bar-chart/stacked-bar-chart';
 import { ChartLoadingAnimation } from './components/chart-loading-animation';
+import { ReactChartsConfigContext, ReactChartsConfigProvider } from './components/react-charts-config';
 
-export { LineChart, BarChart, StackedBarChart, ChartLoadingAnimation };
+export {
+  LineChart,
+  BarChart,
+  StackedBarChart,
+  ChartLoadingAnimation,
+  ReactChartsConfigContext,
+  ReactChartsConfigProvider,
+};
